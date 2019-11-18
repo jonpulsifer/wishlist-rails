33ba58270d20741bb83504912d6f1fe77f523afd
Jonathan Pulsifer <jonathan@pulsifer.ca>
2019-11-18 16:21:09 -0500

    Quick update before it goes live
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,8 @@
 class Gift < ApplicationRecord
     belongs_to :wishlist
     validates :name, presence: true
+
+    def frozen?
+        Date.today < Date.new(2019,12,01)
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.configure do
+    # Whitelist one hostname
+    config.hosts << "wishlist.myshopify.io"
+  end
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
