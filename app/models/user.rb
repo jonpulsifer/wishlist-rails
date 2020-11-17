780e3291b1eb3a3e0852541095fdaea54e3b267c
Jonathan Pulsifer <jonathan.pulsifer@shopify.com>
2020-11-17 18:45:27 -0500

    destroy the right thing
    Signed-off-by: Jonathan Pulsifer <jonathan.pulsifer@shopify.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@
 
 class User < ApplicationRecord
   has_secure_password
-  has_many :family_users
+  has_many :family_users, dependent: :destroy
   has_many :families, through: :family_users
-  has_many :gift_users
-  has_many :gifts, through: :gift_users, dependent: :destroy
+  has_many :gift_users, dependent: :destroy
+  has_many :gifts, through: :gift_users
 
   after_validation :normalize_name, on: [:create, :update]
 
