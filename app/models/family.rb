85ab4ba271b494ee0ae1d9828411871e4c515c1e
Jonathan Pulsifer <jonathan.pulsifer@shopify.com>
2020-11-17 19:15:50 -0500

    ok
    Signed-off-by: Jonathan Pulsifer <jonathan.pulsifer@shopify.com>

--- app/models/family.rb
+++ app/models/family.rb
@@ -4,8 +4,8 @@
 class Family < ApplicationRecord
   has_many :family_users, dependent: :destroy
   has_many :users, through: :family_users
-  has_many :gift_users
-  has_many :gifts, through: :gift_users, source: :user
+  has_many :gift_users, through: :family_users, source: :user
+  has_many :gifts, through: :gift_users
   validates :pin, presence: true, uniqueness: true
 
   def users_except(id)
